514a3f9c0bb98b34e84da31c89af04c65ba650e7
Getaneh Abebe <abebegetaneh.ag@gmail.com>
2022-12-13 18:39:32 +0200

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,36 +22,75 @@ function App() {
   const handleSubmit = (e: React.KeyboardEvent | React.MouseEvent) => {
     e.preventDefault()
     setTodos([...todos, formState])
-    setFormState({ title: '',
-    description: '',
-    status: 'todo'})
+    setFormState({
+      title: '',
+      description: '',
+      status: 'todo'
+    })
 
   }
   return (
     <div className="App">
-      <form>
+      {/* <form>
         <div>
-
           <label htmlFor="title">Title</label>
           <Input id="title" name="title" onChange={handleChange} value={formState.title} />
         </div>
         <div>
-
           <label htmlFor="description">Description</label>
           <Input id="description" name="description" onChange={handleChange} value={formState.description} />
         </div>
-
         <Button type="submit" name='Add' onClick={handleSubmit} value="Add" />
-
-
       </form>
-      <header className="App-header">
+      <div className="App-header">
         {todos.length ? <div>
           {todos.map(({ title, description, status }, index) => <Todo key={`${index}-${status}-${description}-${index}`} status={status} description={description} title={title} />)}
-
         </div> : null}
+      </div> */}
+      <h2>
+        Hello Natan, Yehuda and Abraham ,
+        from now on I will send to you the tasks from this website.
+      </h2>
+      <ul>
+        <h3>
+          Tasks for Natan:
+        </h3>
+        <li>
+          Learn What is funtion in Javascript
+        </li>
+        <li>
+          Create function called 'mult' that recieve 2 numbers as parameters and return the result of them.
+          for example:
+          mult(2, 4) = 8
+          mult=(-1, 0.00000011)=~0
+        </li>
+      </ul>
 
-      </header>
+      <ul>
+        <h3>
+          Tasks for Abraham:
+        </h3>
+        <li>
+          Try to learn CSS & HTML
+        </li>
+        <li>
+          Please build simple website like this and add more styling
+
+        </li>
+      </ul>
+
+      <ul>
+        <h3>
+          Tasks for Yehuda:
+        </h3>
+        <li>
+          Try to learn CSS & HTML
+        </li>
+        <li>
+          Please build simple website like this and add more styling. 
+          call me if you have questions
+        </li>
+      </ul>
     </div>
   );
 }
